3d81376bd550932f242ea5e2a2ac927f01fe6216
Devin Witherspoon <dcwither@gmail.com>
2020-03-03 21:48:51 -0800

    Update goodcheck.rb

--- lib/pronto/goodcheck.rb
+++ lib/pronto/goodcheck.rb
@@ -41,8 +41,7 @@ module Pronto
         next if patch.nil?
 
         line = patch.added_lines.find do |added_line|
-          issue["location"] ||= {}
-          issue["location"]["start_line"] == added_line.new_lineno
+          issue["location"].nil? || issue["location"]["start_line"] == added_line.new_lineno
         end
 
         new_message(line, issue) if line
